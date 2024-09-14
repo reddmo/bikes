ca010d539285487f54e015ff15e68c9758f41ccb
reddmo <126845456+reddmo@users.noreply.github.com>
2024-09-13 20:27:28 -0400

    images

--- eleventy.config.js
+++ eleventy.config.js
@@ -72,13 +72,12 @@ export default async function(eleventyConfig) {
 		// Output formats for each image.
 		formats: ["avif", "webp", "jpg", "png", "auto"],
 
-		widths: [600, "auto"],
+		widths: ["auto"],
 
 		defaultAttributes: {
 			// e.g. <img loading decoding> assigned on the HTML tag will override these values.
 			loading: "lazy",
 			decoding: "async",
-			sizes: "100vmax",
 		}
 	});
 
