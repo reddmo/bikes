a7f4cf56cfdd2e9e67babad5938bf426c9fd39c9
reddmo <126845456+reddmo@users.noreply.github.com>
2024-12-08 11:43:51 -0500

    updates

--- content/_data/webmentions.js
+++ content/_data/webmentions.js
@@ -2,7 +2,7 @@ import EleventyFetch from "@11ty/eleventy-fetch";
 
 export default async function () {
   const WEBMENTIONS_STUFF = process.env.WEBMENTION_IO_TOKEN;
-  const url = `https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_STUFF}&per-page=900`;
+  const url = `https://webmention.io/api/mentions.jf2?token=3AkC5K7teZuoGe3NvSNphw&per-page=900`;
   const res = await EleventyFetch(url, {
     duration: "1h",
     type: "json",
